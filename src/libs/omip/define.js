4bac51d119f7faca78dce0e8caa06fca03d0de9a
dntzhang <dntzhang@qq.com>
2019-03-24 11:38:45 +0800

    rename _weappRef to $scope

--- src/libs/omip/define.js
+++ src/libs/omip/define.js
@@ -18,7 +18,7 @@ export function define(name, ctor) {
   })
 
   config.created = function () {
-    ins._weappRef = this
+    ins.$scope = this
     config.$$refs.forEach(ref => {
       if (ref.type === 'component') {
         if (ref.fn) {
